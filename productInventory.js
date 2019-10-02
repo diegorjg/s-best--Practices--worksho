e70410e7ac18c39edbf4566b410cbb426d8c1a39
diego <diegorjg@gmail.com>
2019-10-02 11:00:16 -0500

    first commit

---
+++ productInventory.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  getProducts: function() { 
+    return products;
+  },
+
+  getProduct: function(productId) {
+    var product = products.find(function(p) { return p.id === productId; });
+    return product;
+  },
+
+};
---
+++ switch_statement.js
@@ -0,0 +1,21 @@
+// COINS:
+// [p]enny
+// [n]ickel
+// [d]ime
+// [q]uarter
+
+var coins = {
+    'p': 1,
+    'n': 5,
+    'd': 10,
+    'q': 25
+  };
+  
+  module.exports = {
+    getAmount: function(coinType) {
+      if(!coins.hasOwnProperty(coinType)){
+        throw new Error('Unrecognized coin ' + coinType);
+      }
+      return coins[coinType];
+    }
+  };
